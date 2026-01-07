6edd6a8befac808c80842823c7300cd9de207293
JEFF7712 <rupanpandyan@gmail.com>
2026-01-07 15:51:10 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+DATA_URL = "https://github.com/rxn4chemistry/rxn_yields/raw/master/data/Buchwald-Hartwig/Dreher_and_Doyle_input_data.xlsx"
+
+df = pd.read_excel(DATA_URL)
+
+print(df.head())
+print(f"Total experiments: {len(df)}")
